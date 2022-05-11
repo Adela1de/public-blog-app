e8e1c225e07c1feb46d3100f250c18d0d8c4724f
Adela1de <94483883+Adela1de@users.noreply.github.com>
2022-05-11 05:40:33 -0300

    Initial commit

---
+++ src/main/java/com/example/publicblogapp/PublicBlogAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.publicblogapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicBlogAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicBlogAppApplication.class, args);
+	}
+
+}
